66c3411af94d62f60f5b586c61e005986140bdc1
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2013-09-03 20:34:01 +0100

    Started to experiment with the gem idea.

--- lib/dottify.rb
+++ lib/dottify.rb
@@ -1,5 +1,17 @@
-require "dottify/version"
+require "./lib/dottify/version"
 
 module Dottify
-  # Your code goes here...
+  module DottedPath
+    def dottified(path)
+      path_head, path_remainder = path.split('.', 2)
+      match = self[path_head]
+      if !match.kind_of?(Hash) || !path_remainder
+        match
+      else
+        match.dottified(path_remainder)
+      end
+    end
+  end
 end
+
+Hash.send :include, Dottify::DottedPath
